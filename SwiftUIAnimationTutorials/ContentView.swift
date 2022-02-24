76f013dfb29a2d7dc147a55c7d65a61f9ee7bc0e
hamitseyrek <hamitseyrek@gmail.com>
2022-02-24 12:57:34 +0300

    Initial Commit

---
+++ SwiftUIAnimationTutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAnimationTutorials
+//
+//  Created by Hamit Seyrek on 24.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAnimationTutorials/SwiftUIAnimationTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationTutorialsApp.swift
+//  SwiftUIAnimationTutorials
+//
+//  Created by Hamit Seyrek on 24.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
