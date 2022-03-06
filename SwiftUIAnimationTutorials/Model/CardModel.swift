33ce14d03f93ba131c8dbc7522d70f4acd2281e5
hamitseyrek <hamitseyrek@gmail.com>
2022-03-06 10:12:31 +0300

    redesign slider area

---
+++ SwiftUIAnimationTutorials/Model/CardModel.swift
@@ -0,0 +1,16 @@
+//
+//  CardModel.swift
+//  SwiftUIAnimationTutorials
+//
+//  Created by Hamit Seyrek on 6.03.2022.
+//
+
+import SwiftUI
+
+struct CardModel: Identifiable {
+    
+    var id = UUID().uuidString
+    var cardColor: Color
+    var offset: CGFloat = 0
+    var title: String
+}
